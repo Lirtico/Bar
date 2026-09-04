b0563d4e4ea1192674c0535a19100b5d3181b4b1
Lirtico <davidestrella773@gmail.com>
2026-09-04 15:21:19 -0600

    primer commit

---
+++ service-worker.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'filez-v2';
+const SHELL_ASSETS = [
+  './',
+  './filez.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './image1.png',
+  './image2.png',
+  './image3.png',
+  './image4.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(SHELL_ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(key) { return key !== CACHE_NAME; })
+            .map(function(key) { return caches.delete(key); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(cached) {
+      return cached || fetch(event.request).then(function(response) {
+        if (response && response.status === 200 && response.type === 'basic') {
+          var clone = response.clone();
+          caches.open(CACHE_NAME).then(function(cache) {
+            cache.put(event.request, clone);
+          });
+        }
+        return response;
+      });
+    }).catch(function() {
+      return caches.match('./filez.html');
+    })
+  );
+});
